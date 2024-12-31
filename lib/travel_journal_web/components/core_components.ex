11cdeb3a6999adb8766a826360e56caef3686f0e
sabyabhoi <sabyabhoi@gmail.com>
2024-12-31 14:13:09 +0530

    Add user authentication

--- lib/travel_journal_web/components/core_components.ex
+++ lib/travel_journal_web/components/core_components.ex
@@ -232,8 +232,8 @@ defmodule TravelJournalWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        "phx-submit-loading:opacity-75 rounded-lg bg-red-500 hover:bg-red-700 py-2 px-3",
+        "text-sm font-semibold leading-6 text-white active:text-white/80 drop-shadow-sm",
         @class
       ]}
       {@rest}
